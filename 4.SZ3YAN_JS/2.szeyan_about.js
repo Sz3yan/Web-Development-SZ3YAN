5f4f062be09e5341e906ef86cb0943e770c7c4d5
Sz3yan <51659380+Sz3yan@users.noreply.github.com>
2021-07-20 11:57:00 +0800

    a

---
+++ 4.SZ3YAN_JS/2.szeyan_about.js
@@ -0,0 +1,90 @@
+function turn1() {
+    document.querySelector(".moreaboutMe__turn1").classList.toggle("turning");
+    document.querySelector(".content1").classList.toggle("blue");
+
+    if (document.querySelector(".content1").classList.contains("blue")) {
+        document.querySelector("#overlay").style.opacity = "1";
+    }
+
+    else {
+        document.querySelector("#overlay").style.opacity = "0";
+    }
+}
+
+function turn2() {
+    document.querySelector(".moreaboutMe__turn2").classList.toggle("turning");
+    document.querySelector(".content2").classList.toggle("blue");
+
+    if (document.querySelector(".content2").classList.contains("blue")) {
+        document.querySelector("#overlay1").style.opacity = "1";
+    }
+
+    else {
+        document.querySelector("#overlay1").style.opacity = "0";
+    }
+}
+
+function turn3() {
+    document.querySelector(".moreaboutMe__turn3").classList.toggle("turning");
+    document.querySelector(".content3").classList.toggle("blue");
+
+    if (document.querySelector(".content3").classList.contains("blue")) {
+        document.querySelector("#overlay2").style.opacity = "1";
+    }
+
+    else {
+        document.querySelector("#overlay2").style.opacity = "0";
+    }
+}
+
+function turn4() {
+    document.querySelector(".moreaboutMe__turn4").classList.toggle("turning");
+    document.querySelector(".content4").classList.toggle("blue");
+
+    if (document.querySelector(".content4").classList.contains("blue")) {
+        document.querySelector("#overlay3").style.opacity = "1";
+    }
+
+    else {
+        document.querySelector("#overlay3").style.opacity = "0";
+    }
+}
+
+function turn5() {
+    document.querySelector(".moreaboutMe__turn5").classList.toggle("turning");
+    document.querySelector(".content5").classList.toggle("blue");
+
+    if (document.querySelector(".content5").classList.contains("blue")) {
+        document.querySelector("#overlay4").style.opacity = "1";
+    }
+
+    else {
+        document.querySelector("#overlay4").style.opacity = "0";
+    }
+}
+
+function turn6() {
+    document.querySelector(".moreaboutMe__turn6").classList.toggle("turning");
+    document.querySelector(".content6").classList.toggle("blue");
+
+    if (document.querySelector(".content6").classList.contains("blue")) {
+        document.querySelector("#overlay5").style.opacity = "1";
+    }
+
+    else {
+        document.querySelector("#overlay5").style.opacity = "0";
+    }
+}
+
+function turn7() {
+    document.querySelector(".moreaboutMe__turn7").classList.toggle("turning");
+    document.querySelector(".content7").classList.toggle("blue");
+
+    if (document.querySelector(".content7").classList.contains("blue")) {
+        document.querySelector("#overlay6").style.opacity = "1";
+    }
+
+    else {
+        document.querySelector("#overlay6").style.opacity = "0";
+    }
+}
