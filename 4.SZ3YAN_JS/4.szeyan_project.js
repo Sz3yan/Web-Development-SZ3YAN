609bfa49745082b0654186f7e033958424a95cfd
Sz3yan <51659380+Sz3yan@users.noreply.github.com>
2021-08-01 01:08:44 +0800

    a

--- 4.SZ3YAN_JS/4.szeyan_project.js
+++ 4.SZ3YAN_JS/4.szeyan_project.js
@@ -3,9 +3,23 @@ function project__details() {
     
     for (let i = 0; i < project.length; i++) {
         project[i].addEventListener("click", function() {
+            var title = document.getElementsByClassName("head");
+            var head = document.querySelector(".project__title");
+            var content = document.querySelector(".project__content");
+
             document.getElementById("hh").classList.toggle("show");
             document.getElementById("jj").classList.toggle("show");
             document.getElementById("kk").classList.toggle("appear");
+            document.getElementById("ph").innerText = title[i].textContent;
+
+            if (title[i].textContent == "SZ3Text. ") {
+                head.textContent = "Creation Think. Edit. Sent.";
+            }
+
+            else if (title[i].textContent == "SZ3Search. ") {
+                head.textContent = "Search Done. Done. Done.";
+                
+            }
         });
     }
     
