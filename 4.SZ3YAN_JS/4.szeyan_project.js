be6545410b4cce7901ebee31dcff4ecb2b2df906
Sz3yan <51659380+Sz3yan@users.noreply.github.com>
2021-08-01 12:24:18 +0800

    a

--- 4.SZ3YAN_JS/4.szeyan_project.js
+++ 4.SZ3YAN_JS/4.szeyan_project.js
@@ -6,19 +6,30 @@ function project__details() {
             var title = document.getElementsByClassName("head");
             var head = document.querySelector(".project__title");
             var content = document.querySelector(".project__content");
+            var content1 = document.querySelector(".content1");
+            var content2 = document.querySelector(".content2");
 
             document.getElementById("hh").classList.toggle("show");
             document.getElementById("jj").classList.toggle("show");
             document.getElementById("kk").classList.toggle("appear");
             document.getElementById("ph").innerText = title[i].textContent;
 
-            if (title[i].textContent == "SZ3Text. ") {
-                head.textContent = "Creation Think. Edit. Sent.";
+            if (title[i].textContent == "SZ3Dev. ") {
+                head.textContent = "Collaboration Snap it. Build it. Send it.";
+                content1.textContent = "Whether you are working alone or with friends, collaboration is key. Share ideas, send giant files or brainstorm on possible solutions. ";
+                content2.textContent = "empowers you to do more.";
+            }
+
+            else if (title[i].textContent == "SZ3Photo. ") {
+                head.textContent = "Creation Think. Edit. Sent";
+                content1.textContent = "Everything is yours to reimagine and remake. Explore newfound hussle and pick up new skills.";
+                content2.textContent = "equips you with the tool. Surprise yourself -- and the world.";
             }
 
             else if (title[i].textContent == "SZ3Search. ") {
                 head.textContent = "Search Done. Done. Done.";
-                
+                content1.textContent = "Redesign from ground up.";
+                content2.textContent = "simply and seamlessly focus on what matters. Search and summarised within seconds. Tune the information presented to you with ease, making anything, easy.";
             }
         });
     }
