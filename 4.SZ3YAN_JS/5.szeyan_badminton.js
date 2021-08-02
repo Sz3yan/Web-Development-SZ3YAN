ff6756c0c9b8a9f7a09e47fa399c5a3474008816
Sz3yan <51659380+Sz3yan@users.noreply.github.com>
2021-08-03 00:31:06 +0800

    a

--- 4.SZ3YAN_JS/5.szeyan_badminton.js
+++
@@ -1,7 +0,0 @@
-function load(){
-    var row=document.getElementById("schedule").insertRow(1);
-	var cell1 = row.insertCell(0);
-	var cell2 = row.insertCell(1);
-	cell1.innerHTML = badminton[0]["name"];
-	cell2.innerHTML = badminton[0].location;
-}
--- 4.SZ3YAN_JS/global.js
+++ 4.SZ3YAN_JS/global.js
@@ -230,6 +230,19 @@ function docIsReady(){
 	}
 	else {	
 		badminton=JSON.parse(badminton);
+        
+        var row = document.getElementById("schedule").insertRow(1);
+
+        for (x = 0; x < badminton.length; x++) {
+            var cell1 = row.insertCell(0);
+            var cell2 = row.insertCell(1);
+            var cell3 = row.insertCell(2);
+            var cell4 = row.insertCell(3);
+            cell1.innerHTML = badminton[x].name;
+            cell2.innerHTML = badminton[x].name;
+            cell3.innerHTML = badminton[x].email;
+            cell4.innerHTML = badminton[x].location;
+        }
 	}
 }
 
