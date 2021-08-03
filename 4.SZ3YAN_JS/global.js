a924318b1833b0273912154ce1790880a7424b7e
Sz3yan <51659380+Sz3yan@users.noreply.github.com>
2021-08-03 18:51:27 +0800

    A

--- 4.SZ3YAN_JS/global.js
+++ 4.SZ3YAN_JS/global.js
@@ -19,27 +19,43 @@ function hide__form() {
     $("#seeAnotherField").change(function() {
         if ($(this).val() == "email") {
             $('#emailshow').show();
+            $('#emailshow input').attr('required','');
+    
             $('#colabshow').hide();
             $('#hishow').hide();
+            $('#colabshow').removeAttr('required');
+            $('#hishow').removeAttr('required');
         } 
     
         else if (($(this).val() == "colab")) {
             $('#colabshow').show();
+            $('#colabshow textarea').attr('required','');
+    
             $('#emailshow').hide();
             $('#hishow').hide();
+            $('#emailshow').removeAttr('required');
+            $('#hishow').removeAttr('required');
         }
     
         else if (($(this).val() == "hi")) {
             $('#hishow').show();
+            $('#hishow textarea').attr('required','');
+    
             $('#emailshow').hide();
             $('#colabshow').hide();        
+            $('#emailshow').removeAttr('required');
+            $('#colabshow').removeAttr('required');
         }
         
         else {
             $('#emailshow').hide();
             $('#colabshow').hide();
             $('#hishow').hide();
+            $('#emailshow').removeAttr('required');
+            $('#colabshow').removeAttr('required');
+            $('#hishow').removeAttr('required');
         }
+    
     });
     
     $("#seeAnotherField").trigger("change");
